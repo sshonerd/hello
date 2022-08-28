8eda4fd76f45e4bf92d15c45793a0c66d01bdec2
Shawn Shonerd <sshonerd2@huskers.unl.edu>
2022-08-28 18:39:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Shawn Shonerd
+ * Date: 2022/08/28
+ *
+ * A simple program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Shawn Shonerd\n");
+
+  return 0;
+}
